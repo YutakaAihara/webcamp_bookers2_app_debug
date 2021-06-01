706cb46f87579be7d713ed05bd3163c76f3aaeba
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-01 07:06:18 +0000

    [Fix]サインアップ後、ログイン後の遷移先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    book_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    book_path(current_user.id)
   end
 
   def configure_permitted_parameters
